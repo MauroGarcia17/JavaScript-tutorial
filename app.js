6af081e2f8a63b337103b9264a6d2adac1a804e3
Mauro Garcia <maurogarciachunga@gmail.com>
2025-06-07 17:09:10 -0500

    Subiendo arhivos del tutorial

---
+++ app.js
@@ -0,0 +1,26 @@
+// tipos de datos
+var saludo = "Hello world"; // string
+'Hello world'; // string
+// console.log(saludo); // imprime "Hello world"
+
+11000// number
+
+true 
+false // boolean
+
+null // null
+
+//array
+//[1, 2, 3, 9]; // array
+//[1, "Hello", true]; // array
+
+
+// object
+let person1 = { name: "John", age: 30 }; // object
+let person2 = { name: "John", age: 30, isStudent: false }; // object
+
+
+// constantes
+const PI = 3.14; // constante
+
+console.log(PI); // imprime 3.14
